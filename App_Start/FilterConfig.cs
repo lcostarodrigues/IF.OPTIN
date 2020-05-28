0a71161026117b4ea81b08c01b214ea06c8adadb
lcostarodrigues <66089685+lcostarodrigues@users.noreply.github.com>
2020-05-28 17:13:36 -0300

    Commit Inicial

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace IF.OPTIN
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
